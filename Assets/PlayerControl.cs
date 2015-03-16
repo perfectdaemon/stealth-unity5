254ad86eef844f3c9ae5c3b8fc8619adefddcc49
perfectdaemon <perfectdaemon@gmail.com>
2015-03-16 17:04:30 +0700

    player animation (idle + walk)

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -3,13 +3,51 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
+    [Range(0.5f, 50.0f)]
+    public float CharacterMaxSpeed;
+
+    public Transform player;
+    public Transform body;
+    public Transform head;    
+    public Camera mainCamera;
+    public Animator animator;
+
+    private Vector2 vMove, vRight;
+
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
+
+    void LookToMouse()
+    {
+        Vector3 dir = mainCamera.ScreenToWorldPoint(Input.mousePosition) - head.position;
+        dir.z = 0;
+        dir.Normalize();
+        Vector3 headDir = head.up;
+        Vector3 bodyDir = body.up;
+        if (Vector3.Dot(dir, bodyDir) > 0.0f)
+            head.up = Vector3.Lerp(headDir, dir, 10 * Time.deltaTime);
+    }
+    
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        LookToMouse();
+        vMove.Set(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        vMove.Normalize();
+
+        animator.SetFloat("CharacterSpeed", vMove.sqrMagnitude);   
+
+        if (vMove.magnitude > 0.5f)
+        {
+            vRight.Set(body.up.x, body.up.y);
+            body.up = Vector2.Lerp(vRight, vMove, 10 * Time.deltaTime);
+        }
+
+        vMove *= CharacterMaxSpeed * Time.deltaTime;
+
+        player.position += new Vector3(vMove.x, vMove.y, 0.0f);                        
 	}
 }
